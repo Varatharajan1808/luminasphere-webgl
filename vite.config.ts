a4532d1062a56b526dbd0a5906cbd17ff4bce9c9
T Varatharajan <tvaratharajan@example.com>
2026-02-25 15:38:26 +0000

    Merge changes safely + +

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,20 @@ export default defineConfig({
     server: {
         port: 3000,
     },
+    plugins: [
+        {
+            name: 'fix-windows-mime-type',
+            configureServer(server) {
+                server.middlewares.use((req, res, next) => {
+                    const url = req.url || '';
+                    if (url.endsWith('.ts')) {
+                        res.setHeader('Content-Type', 'application/javascript');
+                    }
+                    next();
+                });
+            }
+        }
+    ],
     optimizeDeps: {
         force: true
     }
